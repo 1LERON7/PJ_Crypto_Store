cf53b51496c289096788a963035317888b0c2b5e
Vadim <priadkovadim@gmail.com>
2026-02-27 01:21:18 +0200

    UPD: Users and UsersController

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using Crypto_Store.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Crypto_Store.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crypto_Store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public UsersController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserDto dto)
+        {
+            // проверка на пользователя в бд
+            var exists = await _db.Users.AnyAsync(u => u.Email == dto.Email);
+            if (exists)
+                return BadRequest("User already exists");
+
+            var user = new User
+            {
+                Email = dto.Email,
+                PasswordHash = dto.Password,    // ХЭЭШ!!!
+                Role = "user"
+            };
+
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+
+            return Ok(new { user.Id, user.Email });
+
+        }
+
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+
+        [HttpGet("profile")]
+        public IActionResult Profile()
+        {
+            return Ok();
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetUsers()
+        //{
+        //    return Ok();
+        //}
+
+        //[HttpGet("id")]
+        //public async Task<IActionResult> GetUsersById()
+        //{
+        //    return Ok();
+        //}
+
+
+    }
+}
---
+++ DTOs/RegisterUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Crypto_Store.DTOs
+{
+    public class RegisterUserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
