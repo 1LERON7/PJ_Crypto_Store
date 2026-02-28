567ece51feecc717c2dc6bc386633d5d2fcbe728
Vadim <priadkovadim@gmail.com>
2026-02-28 18:31:36 +0200

    UPD: OrderController: full

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -9,6 +9,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Crypto_Store.Controllers
 {
@@ -22,13 +23,75 @@ namespace Crypto_Store.Controllers
             _db = db;
         }
 
+        [Authorize]
+        [HttpPost("/create")]
+        public async Task<IActionResult> CreateOrder(CreateOrdersDto dto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+                return Unauthorized();
 
-        //[Authorize]
-        //[HttpGet("{id}")]
-        //public IActionResult GetMyOrder()
-        //{
+            var products = await _db.Products.Where(p=> dto.ProductIds.Contains(p.Id)).ToListAsync();
+            if (products.Count != dto.ProductIds.Count)
+                return BadRequest("One or more products not found");
 
+            var totalPrice = products.Sum(p=> p.Price);
 
-        //}
+            var order = new order
+            {
+                UserId = Guid.Parse(userId),
+                TotalPrice = totalPrice,
+                Status = "created"
+
+            };
+
+            _db.Orders.Add(order);
+            await _db.SaveChangesAsync();
+
+            var orderItems = products.Select(p => new order_item
+            {
+                OrderId = order.Id,
+                ProductId = p.Id,
+                Price = totalPrice,
+                Quantity = products.Count()
+            });
+
+            // AddRange(); -- добавления массива значений в БД
+            _db.OrderItems.AddRange(orderItems);
+            await _db.SaveChangesAsync();
+
+            return Ok(order);
+        }
+
+        [Authorize]
+        [HttpGet("/my")]
+        public async Task<IActionResult> GetMyOrders(Guid id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if(userId == null) 
+                return Unauthorized();
+
+            var orders = await _db.Orders.Where(o => o.UserId == Guid.Parse(userId)).ToListAsync();
+
+            
+            return Ok(orders);
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderById(Guid id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+                return Unauthorized();
+
+            var order = await _db.Orders.FirstOrDefaultAsync(o=> o.Id == id && o.UserId == Guid.Parse(userId));
+
+            if(order == null) 
+                return NotFound();
+
+            return Ok(order);   
+        }
+
     }
 }
---
+++ DTOs/CreateOrdersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Crypto_Store.DTOs
+{
+    public class CreateOrdersDto
+    {
+        public List<Guid> ProductIds { get; set; } = new();
+
+    }
+}
